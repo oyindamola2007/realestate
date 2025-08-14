d53202a5d823117347b789de5e1c294d103fc297
ESAN OYINDAMOLA <esanayomide2007@gmail.com>
2025-08-14 20:11:39 +0100

    Add files via upload

--- agents/agents.js
+++ agents/agents.js
@@ -18,3 +18,17 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+const hambuger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hambuger.addEventListener("click", () => {
+    hambuger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link"). forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }
+))
